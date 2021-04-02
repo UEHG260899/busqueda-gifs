44febf54572d97464d6d1199a329cac06f526763
Uriel <urielenrique99@gmail.com>
2021-04-02 14:03:11 -0600

    Se agrego persistencia del hisorial de busqueda

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -18,7 +18,9 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+  }
 
   buscarGifs(query: string = '') {
 
@@ -27,6 +29,7 @@ export class GifsService {
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
       this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=trVn8fMme1UuSO8iMf0SmybEWeTdv37o&q=${query}&limit=10`)
